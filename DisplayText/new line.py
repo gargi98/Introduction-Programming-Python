e6090f71dd33130a7256162f475dc2b81fcece4a
GARGI MUKHOPADHYAY <gargiy98@gmail.com>
2018-04-06 16:09:43 +0530

    Create new line.py

---
+++ DisplayText/new line.py
@@ -0,0 +1,10 @@
+#new line using two seperate print statement
+print("Hey there!")
+print("I'm using github!")
+
+#new line using '\n'
+print("Hey there!I'm using github!")
+
+#new line using """
+print("""Hey there!
+    I'm using github!""")
