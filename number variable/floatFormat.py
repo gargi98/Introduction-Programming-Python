7d85a39c1e5f979db6e0ce58b049560c76ca0be2
GARGI MUKHOPADHYAY <gargiy98@gmail.com>
2018-04-07 23:48:41 +0530

    Update floatFormat.py

--- number variable/floatFormat.py
+++ number variable/floatFormat.py
@@ -1,1 +1,8 @@
 
+#print integer number either using %d 
+
+myWeight=54.9
+print("My lucky number is %f" %myWeight)
+
+#or using "{0:d}".format(luckyNumber)
+print("My lucky number is {0:f}".format(myWeight))
