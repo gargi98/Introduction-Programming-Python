26a834d6ea55f1529e7f5bc52428d3360299781b
GARGI MUKHOPADHYAY <gargiy98@gmail.com>
2018-04-07 23:46:59 +0530

    Create intFormat.py

---
+++ number variable/intFormat.py
@@ -0,0 +1,7 @@
+#print integer number either using %d 
+
+luckyNumber=54
+print("My lucky number is %d" %luckyNumber)
+
+#or using "{0:d}".format(luckyNumber)
+print("My lucky number is {0:d}".format(luckyNumber))
